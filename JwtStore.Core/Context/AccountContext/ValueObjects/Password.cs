d3eb84d38d2f542a0e0e9499b1e380c17c250c30
sorza <alexandre.sorza@outlook.com>
2024-11-02 19:28:00 -0300

    Gerador de senhas

--- JwtStore.Core/Context/AccountContext/ValueObjects/Password.cs
+++ JwtStore.Core/Context/AccountContext/ValueObjects/Password.cs
@@ -9,5 +9,24 @@ namespace JwtStore.Core.Context.AccountContext.ValueObjects
 
         public string Hash { get; } = string.Empty;
         public string ResetCode { get; } = Guid.NewGuid().ToString("N")[..8].ToUpper();
+    
+        private static string Generate(
+            short length = 16,
+            bool includeSpecialChars = true,
+            bool upperCase = false)           
+        {
+            var chars = includeSpecialChars ? (Valid + Special) : Valid;
+            var startRandom = upperCase ? 26 : 0;
+            var index = 0;
+            var res = new char[length];
+            var rnd = new Random();
+
+            while (index < length)
+            {
+                res[index++] = chars[rnd.Next(startRandom, chars.Length)];
+            }
+
+            return new string(res);
+        }
     }
 }
