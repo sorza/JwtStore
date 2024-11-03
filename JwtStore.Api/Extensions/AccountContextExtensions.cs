09c640a41b9917e3e2ab5a109cbc60a32f3f676d
sorza <alexandre.sorza@outlook.com>
2024-11-03 19:37:53 -0300

    Criando metodo POST

--- JwtStore.Api/Extensions/AccountContextExtensions.cs
+++ JwtStore.Api/Extensions/AccountContextExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace JwtStore.Api.Extensions
+﻿using JwtStore.Core.Contexts.AccountContext.UseCases.Create;
+using MediatR;
+
+namespace JwtStore.Api.Extensions
 {
     public static class AccountContextExtensions
     {
@@ -21,6 +24,18 @@
         {
             #region Create
 
+            app.MapPost("api/v1/users", async (
+                JwtStore.Core.Contexts.AccountContext.UseCases.Create.Request request,
+                IRequestHandler<
+                    JwtStore.Core.Contexts.AccountContext.UseCases.Create.Request,
+                    JwtStore.Core.Contexts.AccountContext.UseCases.Create.Response> handler) =>
+            {
+                var result = await handler.Handle(request, new CancellationToken());
+
+                return result.IsSuccess
+                    ? Results.Created("",result)
+                    : Results.Json(result, statusCode: result.Status);
+            });
 
 
             #endregion
